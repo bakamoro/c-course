d2998f00fd66d4f9818432f4cad14377151675bf
binyamin <binyaminkeng@gmail.com>
2021-04-28 21:30:19 +0300

    mmn2.1.c

--- mmn2/q1/mmn2.1.c
+++ mmn2/q1/mmn2.1.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
-int main (){
-	printf ("hello word\n"); 
+
+void buffer(int a,char *p){
+	char c;
+	int i = 0;
+	while((scanf("%c",&c) != EOF && p != NULL){
+		p[i] = c;
+		++i;
+		if(i%60 == 0)
+			realloc(p,60*sizeof(char));
+	}
+	return;
+}
+
+void main (){
+	printf ("Please select the desired database type:\nfor a buffer : enter 1\nfor a linked list : enter 2");
+	int a;
+	scanf("%d",&a);
+	char *p = (char *)calloc(60,sizeof(char));
+	if(!p){
+		printf("ERROR");
+		exit(0);
+	}
+	buffer(a,p);
+	if(p == NULL){
+		printf("ERROR");
+                exit(0);
+	}		
+
 }
