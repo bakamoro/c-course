0c478844cfbf8a73aeacc346833d6df3bc6395c4
binyamin <binyaminkeng@gmail.com>
2021-04-29 11:29:00 +0300

    mmn2.1.c

--- mmn2/q1/mmn2.1.c
+++ mmn2/q1/mmn2.1.c
@@ -5,10 +5,12 @@ void buffer(char *p){
 	int i = 0;
 	printf("please write your text\n");
 	while((scanf("%c",&c)) != EOF && p != NULL){
-		p[i] = c;
-		++i;
-		if(i%60 == 0)
-			realloc(p,60*sizeof(char));
+		if(c != "\n"){
+			p[i] = c;
+			++i;
+			if(i%60 == 0)
+				realloc(p,60*sizeof(char));
+		}
 	}
 	return;
 }
@@ -34,10 +36,31 @@ void readText(int a , char *p){
                 }
                 else printf("ERROR you didn't entered 1 or 2\n");
         }
+	return;
 }
+void print_buffer(char *p){
+	int i = 0;
+	printf("you wrote:\n");
+	while(p[i] != EOF){
+		printf("%c",p[i]);
+		i++;
+		if((i % 60) == 0)
+			printf("\n");
+	}
+	return;
+}
+
+void print_linked_list(){
+
+}
+
 void printText(int a,char *p){
-	
+	if(a == 1)
+		print_buffer(p);
+	if(a == 2)
+		print_linked_list();	
 }
+
 void main (){
 	printf ("Please select the desired database type:\nfor a buffer : enter 1\nfor a linked list : enter 2\n");
 	int a;
