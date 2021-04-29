38e9bdc5a83a701ec411ee6f73b2c6626a396eca
binyamin <binyaminkeng@gmail.com>
2021-04-29 10:59:29 +0300

    mmn2.1.c

--- mmn2/q1/mmn2.1.c
+++ mmn2/q1/mmn2.1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-void buffer(int a,char *p){
+void buffer(char *p){
 	char c;
 	int i = 0;
-	while((scanf("%c",&c) != EOF && p != NULL){
+	printf("please write your text"):
+	while((scanf("%c",&c)) != EOF && p != NULL){
 		p[i] = c;
 		++i;
 		if(i%60 == 0)
@@ -11,20 +12,39 @@ void buffer(int a,char *p){
 	}
 	return;
 }
+void linked_list(int *p){
+	
+}
+void readText(int a , char *p){
+	if(a == 1){
+                *p = (char *)calloc(60,sizeof(char));
+                if(!p){
+                        printf("ERROR");
+                        exit(0);
+                }
+                buffer(a,p);
+                if(p == NULL){
+                        printf("ERROR");
+                        exit(0);
+                }
+        }
+        else{ 
+                if(a == 2){
 
+                }
+                else printf("ERROR you didn't entered 1 or 2");
+        }
+}
+void printText(int a,*p){
+	
+}
 void main (){
 	printf ("Please select the desired database type:\nfor a buffer : enter 1\nfor a linked list : enter 2");
 	int a;
+	char *p;
 	scanf("%d",&a);
-	char *p = (char *)calloc(60,sizeof(char));
-	if(!p){
-		printf("ERROR");
-		exit(0);
-	}
-	buffer(a,p);
-	if(p == NULL){
-		printf("ERROR");
-                exit(0);
-	}		
-
+	readText(a,*p);
+	if(!p) 
+		return;
+	printText(a,*p);
 }
