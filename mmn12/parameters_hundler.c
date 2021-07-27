0174232da72f9deaef11c3f8405fc6dbc523054e
bakamoro <binyaminkenig@gmail.com.com>
2021-07-27 15:33:14 +0300

    parameters_hundler.c

---
+++ mmn12/parameters_hundler.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "read.h"
+#include "label_handler.h"
+//
+void instructions_2(char line[81],char instruction[],int line_num,int index){
+    char label[MAX_LABLE_SIZE];
+    int num_of_pa = 0;
+    int i = 0;
+    while(check_rest_of_line(line,index)){
+        index = bilt_array(line,label,index);
+        num_of_pa++;
+        if(num_of_pa == 2){
+            printf("ERROR - LINE : %d - TO MANY PARAMETERS\n",line_num);
+            return;
+        }
+        if(legal_label(instruction,line_num)){
+            if((i = search_lable(label,line_num))){
+                lable_table[i].called = 'y';
+                if(instruction == ".extern"){
+                    lable_table->ex = 'y';
+                }
+                else lable_table->ex = 'n';
+            }
+            add_lable(label,'y',index_lable,line_num);
+        }
+    }
+    if(num_of_pa == 0){
+        printf("ERROR - LINE : %d - TO FEW PARAMETERS\n ",line_num);
+        return;
+    }
+}
+void instructions_1(char word[]){
+
+}
