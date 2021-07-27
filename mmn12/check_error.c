2e02cc1df64b5a75be4e03cc0d2c803681e9f462
bakamoro <binyaminkenig@gmail.com.com>
2021-07-27 15:25:19 +0300

    check_error.c

--- mmn12/check_error.c
+++ mmn12/check_error.c
@@ -4,8 +4,9 @@
 #include <string.h>
 #include "hash_table.h"
 #include "line_handler.h"
+#include "label_handler.h"
 #define MAX_LINE_SIZE  81
-
+extern int index_lable;
 //make a line from array of chasrs
 void read_line(FILE *fd,char p[81]){
 	int i = 0;
@@ -22,7 +23,7 @@ int number_check(char s[],int line_number){
 	int i = 0;
 	while(i < strlen(s)){
 		if(s[i] == '.'){
-			printf("ERROR - line - %c - the asembler does not support decimal numbers\n",line_number);
+			printf("ERROR - line : %c - the asembler does not support decimal numbers\n",line_number);
 			return 0;
 		}
 		if(!(isdigit(s[i]))){
@@ -41,6 +42,7 @@ void main_check(char s[]){
 	char c;
 	char *p;
 	int line_num = 1;
+	int i = 0,j = 0;
 	if(fd == NULL){
 		printf("ERROR - can't open the file - %s\n",s);
 		exit(0);
@@ -65,7 +67,8 @@ void main_check(char s[]){
 			fsetpos(fd,&pos);
 			read_line(fd,p);
 			//לשלוח לפונקציה מקובץ אחר לא לשכוח לשלוח את מספר השורה.
-			int a = main_line(p,line_num,0,0);
+			char *la;
+			int a = main_line(p,la,line_num,0,0);
 			if (!(a))
 			{
 				printf("ERROR - LINE NUMBER : %d - NO INSTRUCTUON OR COMAND FOUND\n",line_num);
@@ -73,6 +76,18 @@ void main_check(char s[]){
 			free(p);
 			line_num++;
 		}
+	}
+	//chcking that all the labels are called.
+	//lable * lable_table2 = get_lable_table();
+	while(i < index_lable){
+		if(lable_table[i].called == 'n'){
+			j = 0;
+			while(j < lable_table->line_size){
+				printf("ERROR - LINE : %d - UNDIFIND LABLE - %s\n",lable_table->line[j],lable_table->name);
+				j++;
+			}
+		}
+		i++;
 	}
 	fclose(fd);
 }
