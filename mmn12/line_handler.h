00cbfb9e7bc09afdcec5b19be13a23283570359c
bakamoro <binyaminkenig@gmail.com.com>
2021-07-27 15:32:25 +0300

    line_handler.h

---
+++ mmn12/line_handler.h
@@ -0,0 +1,8 @@
+#ifndef LINE_HANDLER
+#define LINE_HANDLER
+int in_comands_R(char word[]);
+int in_comands_I(char word[]);
+int in_comands_J(char word[]);
+int in_instructions_1(char word[]);
+int main_line(char *line,char *label,int line_num,int index,int a);
+#endif
