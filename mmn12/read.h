27532bf41997bc8a6e5038b500d5e07baa5b010d
bakamoro <binyaminkenig@gmail.com.com>
2021-07-27 15:34:21 +0300

    read.h

---
+++ mmn12/read.h
@@ -0,0 +1,12 @@
+#ifndef READ
+#define READ
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define MAX_LINE_SIZE 81
+#define MAX_WORD_SIZE 81
+int bilt_array(char p[],char p3[],int i);
+int check_rest_of_line(char line[MAX_LINE_SIZE],int index);
+int operand_chack(char * op,int line_num);
+#endif
