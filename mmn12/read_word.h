9516cea6ba3868a44e526ba069b23cddd72e7450
bakamoro <binyaminkenig@gmail.com.com>
2021-07-29 13:25:09 +0300

    read_word.h

--- mmn12/read_word.h
+++ mmn12/read_word.h
@@ -1,6 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define MAX_LINE_SIZE 81
+#define MAX_WORD_SIZE 81
+int check_rest_of_line(char line[MAX_LINE_SIZE],int index){
+	while(line[index] == ' ' || line[index] == '\t');
+	if(line[index] == '\n'){
+		return 1;
+	}
+	return 0;
+}
+int operand_chack(char * op,int line_num){
+	if(op[0] != '$'){
+		printf("ERROR LINE : %d - ILLEGLE HOARDER",line_num);
+		return 0;
+	}
+	if(atoi(strstr(op,'$')) > 31){
+		printf("ERROR LINE : %d - ILLEGLE HOARDER",line_num);
+		return 0;
+	}
+	
+}
 //make a word from array of chasrs
-#define MAX_WORD_SIZE  81
-int bilt_array(char p[],char p3[],int i){
+int bilt_array(char p[],char p3[81],int i){
 	int j = 0;
     while(i < MAX_WORD_SIZE && p[i] == ' ' || p[i] == '\n' || p[i] == '\t'){
         i++;
