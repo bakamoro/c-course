17a8611bda5540de4ee5766e7106266d991e2c74
shimiBK <37695804+shimiBK@users.noreply.github.com>
2021-08-18 23:52:52 +0300

    fixed methods

--- assembler/firstpass.h
+++ assembler/firstpass.h
@@ -1,9 +1,22 @@
 #pragma once
 #include "secondpass.h"
 
-void proccessGuidance(char* token ,int* Address,int* DC,int* LC,nodeMachine** ptr,char* machCode);
+/*
+ *This Method is proccesing guidance commands and adding their binary code to the Machine code Table
+ *This @param token is the command
+ *This @param DC is the Data counter
+ *This @param LC is the line counter
+ *This @param ptr is a pointer to the machine code Table
+ *This param machCode is the array of the binary string to be added to the machine code table
+ */
 
-int proccessInstruction(char* token ,int* Address,int* LC ,nodeSymbol** head,nodeMachine** ptr,char* machCode);
+
+void proccessGuidance(char* token ,int* DC,int* LC,nodeMachine** ptr,char* machCode);
+
+/*
+ * this method is proccesing Instructions commands and adding their binary code to the machine code table
+ */
+int proccessInstruction(char* token ,int* LC ,int* IC , nodeSymbol** head,nodeMachine** ptr,char* machCode);
 
 /*
  * first pass on the input file , setting all the labels in the symbol table , creating the machine code table
