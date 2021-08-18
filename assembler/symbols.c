956e96295c6072017f461150aa0bed79cb58a4d0
shimiBK <37695804+shimiBK@users.noreply.github.com>
2021-08-18 23:57:38 +0300

    fixed methods

--- assembler/symbols.c
+++ assembler/symbols.c
@@ -18,129 +18,159 @@
 void addSymbolNode(nodeSymbol** p_head, char* name, int memValue, char* attribute)
 {
 	int i;
-    if ((p_head == NULL) || (name == NULL))
-    {
-        return;
-    }
+	if ((p_head == NULL) || (name == NULL))
+	{
+		return;
+	}
 
 
-    if (*p_head == NULL)
-    {
-        *p_head = (nodeSymbol*)malloc(sizeof(nodeSymbol));
+	if (*p_head == NULL)
+	{
+		*p_head = (nodeSymbol*)malloc(sizeof(nodeSymbol));
 
-        if (*p_head == NULL)
-        {
-            printf("Unable to Allocate Memory");
+		if (*p_head == NULL)
+		{
+			printf("Unable to Allocate Memory");
 
-        }
-        else
-        {
-            (*p_head)->next = NULL;
+		}
+		else
+		{
+			(*p_head)->next = NULL;
 
-        }
-    }
-    else
-    {
-        nodeSymbol* temp = (nodeSymbol*)malloc(sizeof(nodeSymbol));
-        if (temp == NULL)
-        {
-            printf("Unable to Allocate Memory");
+		}
+	}
+	else
+	{
+		nodeSymbol* temp = (nodeSymbol*)malloc(sizeof(nodeSymbol));
+		if (temp == NULL)
+		{
+			printf("Unable to Allocate Memory");
 
-        }
-        else
-        {
-            temp->next = *p_head;
-            *p_head = temp;
+		}
+		else
+		{
+			temp->next = *p_head;
+			*p_head = temp;
 
-        }
-    }
-    strcpy((*p_head)->name, name);
-    (*p_head)->memValue = memValue;
-    for (i = 0; i < 2; i++)
-    {
-        memset(&(*p_head)->attributes[i], 0, 7);
-    }
+		}
+	}
+	strcpy((*p_head)->name, name);
+	(*p_head)->memValue = memValue;
+	for (i = 0; i < 2; i++)
+	{
+		memset(&(*p_head)->attributes[i], 0, 7);
+	}
 
-    if (!strcmp(attribute, "entry") || (!strcmp(attribute, "extern"))) /*if external or entry put in the second place*/
-    {
-        strcpy((*p_head)->attributes[1], attribute);
-    }
-    else {
-        strcpy((*p_head)->attributes[0], attribute);
-    }
+	if (!strcmp(attribute, "entry") || (!strcmp(attribute, "extern"))) /*if external or entry put in the second place*/
+	{
+		strcpy((*p_head)->attributes[1], attribute);
+	}
+	else {
+		strcpy((*p_head)->attributes[0], attribute);
+	}
 }
 int searchSymbol(nodeSymbol** p_head, char* name)
 {
-    struct nodeSymbol* ptr = (*p_head);
+	struct nodeSymbol* ptr = (*p_head);
 
-    if (ptr == NULL)
-        return 0;
+	if (ptr == NULL)
+		return 0;
 
 
-    while (ptr!= NULL)
-    {
-        if (!comper_words(ptr->name, name))
-        {
-            return 1;
+	while (ptr!= NULL)
+	{
+		if (!strcmp(ptr->name, name))
+		{
+			return 1;
 
-        }
-        ptr = ptr->next;
-    }
+		}
+		ptr = ptr->next;
+	}
 
-    return 0;
+	return 0;
 }
 
 int returnAddress(nodeSymbol** p_head, char* name)
 {
 	struct nodeSymbol* ptr = (*p_head);
 
-    if ((p_head == NULL) || (name == NULL))
-    {
-        return 0;
-    }
+	if ((p_head == NULL) || (name == NULL))
+	{
+		return 0;
+	}
 
 
-    if ((*p_head) == NULL)
-        return 0;
+	if ((*p_head) == NULL)
+		return 0;
 
-    while (ptr != NULL)
-    {
-        if (!comper_words(ptr->name, name))
-        {
-            return ptr->memValue;
+	while (ptr != NULL)
+	{
+		if (!strcmp(ptr->name, name))
+		{
+			return ptr->memValue;
 
-        }
-        ptr = ptr->next;
+		}
+		ptr = ptr->next;
 
 
-    }
+	}
 
-    return 0;
+	return 0;
 
 }
 
 void addAttribute(nodeSymbol** p_head, char* name, char* attribute)
 {
 
-    struct nodeSymbol* ptr = (*p_head);
+	struct nodeSymbol* ptr = (*p_head);
 
-    if (ptr == NULL)
-    {
+	if (ptr == NULL)
+	{
 
-        printf("ERROR");
-    }
+		printf("ERROR");
+	}
 
 
-    while (ptr != NULL)
-    {
-        if (!strcmp(ptr->name, name))
-        {
-            strcpy(ptr->attributes[1], attribute);
+	while (ptr != NULL)
+	{
+		if (!strcmp(ptr->name, name))
+		{
+			strcpy(ptr->attributes[1], attribute);
 
-        }
-        ptr = ptr->next;
-    }
+		}
+		ptr = ptr->next;
+	}
 
 
 
 }
+
+void updateAddress(nodeSymbol** p_head , int* ICF)
+{
+	struct nodeSymbol* ptr = (*p_head);
+
+	if (ptr == NULL)
+	{
+
+		printf("ERROR");
+	}
+
+
+	while (ptr != NULL)
+	{
+
+	 if(!strcmp(ptr->attributes[0],"data"))
+		{
+
+			ptr->memValue = ptr->memValue + *ICF;
+
+		}
+
+		ptr = ptr->next;
+	}
+}
+
+
+
+
+
