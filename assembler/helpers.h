767141fe215bcbe50669b7bd311d78f1b32f2f1e
shimiBK <37695804+shimiBK@users.noreply.github.com>
2021-08-18 11:34:06 +0300

    updated fucntions

--- assembler/helpers.h
+++ assembler/helpers.h
@@ -2,16 +2,45 @@
 
 
 
+
+
 /*
- * Converts decimal number to binary
- * @param n the decimal number to be converted
- * @param k is the length of the binary number
+
+ * Check if a number is exceeding its limits
+
+ * @param num is the number to be checked
+
  */
+
+
+
+int checkLimits(int num);
+
+
+
+/*
+
+ * Converts decimal number to binary
+
+ * @param n the decimal number to be converted
+
+ * @param k is the length of the binary number
+
+ */
+
 char* decimal_to_binary(int n, int k);
 
+
+
 /*
+
  * Converts a String of binary numbers to string of HEx
+
  * @param binarySting is the string of the binary number
+
  */
 
+
+
 char* convertToHex(char* binaryString);
+
